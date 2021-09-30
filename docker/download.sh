181993855a0dbaee28e7743212cf6f0c5a66cea6
Joker <37207427+NGyokchakyan@users.noreply.github.com>
2021-09-30 20:48:21 +0400

    Delete docker directory

--- docker/download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -O mail.html  www.mail.ru
-wget -O fb.html  www.fb.com
-wget -O lessson.html  www.lesson.ml
-wget -O youtube.html www.youtube.com
--- docker/script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo  $(cat /app/test /app/test2)
