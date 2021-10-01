3900bc98adaa0d840fb035569197bfe0171b6ca6
Narek <narek.gyokchakyan@gmail.com>
2021-10-01 13:10:36 +0000

    add homworks

--- check_ip.sh
+++ check_ip.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-
-function ip.addr {
-
-#arr=(eno eth wlo)
-for i in ${arr[*]}; do ip a | grep $i"[0-9]" |awk '{print $2}'| sed 'N;s/\n/ ip addr - /';done
+function scaner {
+ip route show | grep "^[0-9]" | awk '{print $3 " is ip addr" " -- " $1}'
 }
-arr=$*
 
-ip.addr $arr
-#echo $?
+if [[ $# -gt 0 ]]
+then 
+for i in $@ 
+do scaner | grep -q $i 
+if [[ $? -eq 0 ]]
+then
+scaner | grep  $i
+else
+for n in $i 
+do echo "$n interface not found"
+done
+fi
+done
+else
+scaner
+fi
